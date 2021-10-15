f02e71d950ef06c3a97398f8ccfa9a14d8a87616
Winry <winry.tian@gmail.com>
2021-10-15 13:57:03 -0700

    add template project

---
+++ sketch.js
@@ -0,0 +1,19 @@
+/*
+@author 
+@date 2021-
+
+ */
+let font
+
+function preload() {
+    font = loadFont('fonts/Meiryo-01.ttf')
+}
+
+function setup() {
+    createCanvas(640, 360)
+    colorMode(HSB, 360, 100, 100, 100)
+}
+
+function draw() {
+    background(209, 80, 30)
+}
