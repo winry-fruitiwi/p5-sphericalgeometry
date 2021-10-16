18f44877c3bfa90bdf7b8030350d435f3f9bbab2
Winry <winry.tian@gmail.com>
2021-10-16 13:33:47 -0700

    add coding plan

--- sketch.js
+++ sketch.js
@@ -1,8 +1,19 @@
 /*
-@author 
-@date 2021-
+@author Winry
+@date 2021-10-16
 
- */
+Coding plan:
+    EasyCam so that I can actually work with spheres
+    Convert into WEBGL and draw axes
+    HUD for X, Y, and Z axes with Blender view
+    Explain 3D polar coordinates at the top of the program (need more knowledge)
+    Draw points with X and Y, like latitude and longitude
+    Create 2D array and stuff points into it, then show every point
+    Try to make the triangle strips
+    Make a rainbow! Also add code for random points, if wanted. Makes sound!
+    Maybe try a quadrilateral approach? That would satisfy Adam.
+*/
+
 let font
 
 function preload() {
