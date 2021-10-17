7e94c4d2583862607fe68aceb6988ad234555548
Winry <winry.tian@gmail.com>
2021-10-17 14:09:17 -0700

    add drawAxes function, wireframed sphere

--- sketch.js
+++ sketch.js
@@ -15,6 +15,32 @@ Coding plan:
 */
 
 let font
+const DARK_BRIGHTNESS = 60
+const LIGHT_BRIGHTNESS = 100
+
+function drawAxes() {
+    strokeWeight(2)
+    // positive x-axis
+    stroke(0, 100, LIGHT_BRIGHTNESS)
+    line(0, 0, 4000, 0)
+    // negative x-axis
+    stroke(0, 100, DARK_BRIGHTNESS)
+    line(-4000, 0, 0, 0)
+
+    // positive y-axis
+    stroke(90, 100, LIGHT_BRIGHTNESS)
+    line(0, 0, 0, 4000)
+    // negative y-axis
+    stroke(90, 100, DARK_BRIGHTNESS)
+    line(0, -4000, 0, 0)
+
+    // positive z-axis
+    stroke(220, 100, LIGHT_BRIGHTNESS)
+    line(0, 0, 0, 0, 0, 4000)
+    // negative z-axis
+    stroke(220, 100, DARK_BRIGHTNESS)
+    line(0, 0, -4000, 0, 0, 0)
+}
 
 function preload() {
     font = loadFont('fonts/Meiryo-01.ttf')
@@ -28,5 +54,12 @@ function setup() {
 
 function draw() {
     background(209, 80, 30)
+    noFill()
+    // turn this to 0.01 for a scary spiderweb globe effect!
+    strokeWeight(0.1)
+    // you need 100 brightness or else the spiderweb won't be visible
+    stroke(0, 0, 50)
     sphere(100)
+
+    drawAxes()
 }
